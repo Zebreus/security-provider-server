3a3b4df145eb9de3f1ee183eab7fc5cda31e6f7f
Lennart Eichhorn <lennart@madmanfred.com>
2020-07-28 13:37:36 +0200

    Changed server to obtain public key from security provider

--- security-server.pro
+++ security-server.pro
@@ -3,6 +3,7 @@ QT -= gui
 CONFIG += c++11 console
 CONFIG -= app_bundle
 
+include($$PWD/libs/security-provider/client/client.pri)
 include($$PWD/libs/qt-jsonrpc-server/qt-jsonrpc-server.pri)
 
 # The following define makes your compiler emit warnings if you use
--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,25 @@
 #include <QFileInfo>
 #include <server.h>
 #include "demoserver.h"
+#include <client.h>
+
+jsonrpc::Server<DemoServer> server(9093);
+QString publicKey;
+
+void startServerWithKey(const QString& key){
+    publicKey = key;
+    server.setConstructorArguments(publicKey);
+    server.startListening();
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    //TODO get public key from provider
-    QFileInfo publicKeyPath("../securityProvider/res/public_key.pem");
-
-    QFile publicKeyFile(publicKeyPath.absoluteFilePath());
-
-    bool error = false;
-    if (!publicKeyFile.open(QFile::ReadOnly | QFile::Text)){
-        qDebug() << "Error opening publickey file: " << publicKeyPath.absoluteFilePath();
-        error = true;
-    }
-    if(error){
-        exit(1);
-    }
-
-    QString publicKey = QTextStream(&publicKeyFile).readAll();
-
-    jsonrpc::Server<DemoServer> server(9093);
-    server.setConstructorArguments(publicKey);
-    server.startListening();
+    securityprovider::Client client;
+    QObject::connect(&client, &securityprovider::Client::gotPublicKey, startServerWithKey);
+    client.open(QUrl("ws://localhost:9092"));
+    client.getPublicKey();
 
     return a.exec();
 }
